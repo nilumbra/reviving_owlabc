8882deee9b97ae35390e7209169799bfcbaad7fb
Lucija Gregov <lgregov@gmail.com>
2015-08-24 23:47:33 +0100

    Moved js to a .js file

--- releases/20150812153807/app/assets/javascripts/calendar.js
+++ releases/20150812153807/app/assets/javascripts/calendar.js
@@ -1,3 +1,10 @@
 //= require moment
 //= require fullcalendar
 
+$(function() {
+    // page is now ready, initialize the calendar...
+
+    $('#calendar').fullCalendar({
+        // put your options and callbacks here
+    })
+});
