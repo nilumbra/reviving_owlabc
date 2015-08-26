8dc8351b4209e597ca95dd25233a2fcf729178d6
Lucija Gregov <lgregov@gmail.com>
2015-08-26 11:38:16 +0100

    Remove migration

--- releases/20150812153807/db/migrate/20150825175822_add_start_and_end_day_to_units.rb
+++
@@ -1,6 +0,0 @@
-class AddStartAndEndDayToUnits < ActiveRecord::Migration
-  def change
-    add_column :units, :start_day, :date
-    add_column :units, :end_day, :date
-  end
-end
--- releases/20150812153807/db/schema.rb
+++ releases/20150812153807/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825175822) do
+ActiveRecord::Schema.define(version: 20150722065332) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -144,8 +144,6 @@ ActiveRecord::Schema.define(version: 20150825175822) do
     t.string   "pdf_content_type", limit: 255
     t.integer  "pdf_file_size",    limit: 4
     t.datetime "pdf_updated_at"
-    t.date     "start_day"
-    t.date     "end_day"
   end
 
   add_index "units", ["course_id"], name: "index_units_on_course_id", using: :btree
